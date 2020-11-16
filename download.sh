62da22db7d68ca73934a4f69406b1c95640ed8ca
Bruno Praciano <bjpraciano@gmail.com>
2020-11-16 18:17:19 +0100

    Initial commit

---
+++ download.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+python2 Exporter.py --querysearch "ciro gomes" --output ciroAposDebate.csv --maxtweets 100 --toptweets
+python2 Exporter.py --querysearch "marina silva" --output marinaAposDebate.csv --maxtweets 100 --toptweets
+python2 Exporter.py --querysearch "alvaro dias" --output alvaroAposDebate.csv --maxtweets 100 --toptweets
+python2 Exporter.py --querysearch "bolsonaro" --output bolsonaroAposDebate.csv --maxtweets 100 --toptweets
+python2 Exporter.py --querysearch "cabo daciolo" --output caboAposDebate.csv --maxtweets 100 --toptweets
+python2 Exporter.py --querysearch "alckmin" --output alckminAposDebate.csv --maxtweets 100 --toptweets
+python2 Exporter.py --querysearch "boulos" --output boulosAposDebate.csv --maxtweets 100 --toptweets
+python2 Exporter.py --querysearch "henrique meirelles" --output henriqueAposDebate.csv --maxtweets 100 --toptweets
+
+
+
